bd451cafacd65acdd93f62ac67db50b9ec82d6cb
rapstar-surya <surajchaudhari357@gmail.com>
2023-03-07 00:17:05 +0530

    My React - Challenge One

--- src/App.js
+++ src/App.js
@@ -3,21 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+     <h1 align="left">I am heading one ... from first component</h1>
+     <h2 align="left">I am heading two ... from second component</h2>
+     <h3 align="left">I am heading three ... from third component</h3>
+     <button id="button">Click Me!!</button>
     </div>
   );
 }
